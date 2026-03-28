d14049243223b30d049f48909fbb1040cb2af439
showusyourtalent <showusyourtalent2@gmail.com>
2026-03-28 23:49:23 +0100

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://show-us-your-talent-backend-1.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://showusyourtalent-zvnl.vercel.app';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
